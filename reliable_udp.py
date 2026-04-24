12e4ec8f7b1146c130e21793e2ffa84c0325c279
Marwan <marwanelgharbawy2@gmail.com>
2026-04-24 19:20:36 +0300

    Add error simulation

--- reliable_udp.py
+++ reliable_udp.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import random
 
 class ReliableUDP:
     def __init__(self, host, port, is_server=False):
@@ -7,13 +8,21 @@ class ReliableUDP:
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP socket
         
-        # unsigned int for seq_num, unsigned int for ack_num, unsigned char for 3 flags, unsigned short for checksum
+        # unsigned int for seq_num (4 bytes)
+        # unsigned int for ack_num (4 bytes)
+        # unsigned char for 3 flags (1 byte)
+        # unsigned short for checksum (2 bytes)
         self.header_format = '!I I B H'
+        
         self.seq_num = 0
         self.ack_num = 0
+        
         self.expected_seq_num = 0
         
+        # simulation paramteres: packet loss probabilites and data corruption
         self.sock.settimeout(1.0) 
+        self.packet_loss_prob = 0.1
+        self.data_corruption_prob = 0.1
         
         # if not server, no need to bind
         if is_server:
@@ -27,7 +36,13 @@ class ReliableUDP:
         packet = self._create_packet(self.seq_num, 0, 0, data)
         
         while True:
-            self.sock.sendto(packet, address)
+            
+            if self._simulate_loss():
+                pass # do nothing
+            else:
+                # simulate checksum corruption
+                packet_to_send = self._simulate_false_checksum(packet)
+                self.sock.sendto(packet_to_send, address)
             
             # block until ACK is received or timeout occurs
             try:
@@ -71,7 +86,12 @@ class ReliableUDP:
                         
                         # send ACK packet with ACK flag set and ACK number equal to received seq_num
                         ack_packet = self._create_packet(0, seq_num, 1)
-                        self.sock.sendto(ack_packet, address)
+                        
+                        # simulate ACK packet loss
+                        if self._simulate_loss():
+                            pass # do nothing
+                        else:
+                            self.sock.sendto(ack_packet, address)
                         
                         # if it's the expected sequence number
                         if seq_num == self.expected_seq_num:
@@ -80,6 +100,25 @@ class ReliableUDP:
                         # else, it's duplicate packet
             except socket.timeout:
                 continue
+            
+    def _simulate_packet_loss(self):
+        return random.random() < self.packet_loss_prob
+    
+    # modify the checksum to simulate data corruption
+    def _simulate_false_checksum(self, packet):
+        if random.random() < self.data_corruption_prob:
+            
+            # change type of packet to bytearray then put it back to bytes after modification
+            corrupted = bytearray(packet)
+            
+            # The checksum starts at byte index (4 + 4 + 1)
+            # target the checksum byte and toggle the whole byte
+            corrupted[9] ^= 0xFF 
+            
+            return bytes(corrupted)
+        return packet # just return normal packet 
+                
+        return data
 
     def _calculate_checksum(self, data):
         
@@ -99,11 +138,13 @@ class ReliableUDP:
 
     # private method
     def _create_packet(self, seq_num, ack_num, flags, data=b''):
+        
         # packet structure: header + data
         # header: seq_num, ack_num, flags, checksum (needs to be calculated)
         temp_header = struct.pack(self.header_format, seq_num, ack_num, flags, 0)
         checksum = self._calculate_checksum(temp_header + data)
         header = struct.pack(self.header_format, seq_num, ack_num, flags, checksum)
+        
         return header + data
 
     def _parse_packet(self, packet):
@@ -119,4 +160,5 @@ class ReliableUDP:
         
         # unpack using the same format as packing to get header data
         seq_num, ack_num, flags, recv_checksum = struct.unpack(self.header_format, header)
+        
         return seq_num, ack_num, flags, recv_checksum, data
